e8f2d13e4f3e484ab36390a70e8363b125c917f6
@hexlet/cli <support@hexlet.io>
2023-04-01 20:46:10 +0600

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -8,41 +8,6 @@ import java.util.Map.Entry;
 
 // BEGIN
 class App {
-    public static void main(String[] args) {
-        List<Map<String, String>> books = new ArrayList<>();
-
-        Map<String, String> book1 = new HashMap<>(
-                Map.of("title", "Cymbeline", "author", "Shakespeare", "year", "1611")
-        );
-        Map<String, String> book2 = new HashMap<>(
-                Map.of("title", "Book of Fooos", "author", "FooBar", "year", "1111")
-        );
-        Map<String, String> book3 = new HashMap<>(
-                Map.of("title", "The Tempest", "author", "Shakespeare", "year", "1611")
-        );
-        Map<String, String> book4 = new HashMap<>(
-                Map.of("title", "Book of Foos Barrrs", "author", "FooBar", "year", "2222")
-        );
-        Map<String, String> book5 = new HashMap<>(
-                Map.of("title", "Still foooing", "author", "FooBar", "year", "3333")
-        );
-
-        books.add(book1);
-        books.add(book2);
-        books.add(book3);
-        books.add(book4);
-        books.add(book5);
-
-        //System.out.println(books);
-
-        Map<String, String> where = new HashMap<>(Map.of("author", "Shakespeare", "year", "1611"));
-        List<Map<String, String>> result = App.findWhere(books, where);
-
-        //System.out.println(result);
-
-        //System.out.println("Final: " + App.findWhere(books, where));
-    }
-
     public static List<Map<String, String>> findWhere(List<Map<String, String>> listOfBooks,
                                                       Map<String, String> whereBooks) {
         List<Map<String, String>> result = new ArrayList<>();
